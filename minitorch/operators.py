a47f053460379b3596744c719c0ae54d610433b0
lockshin-miracle <lockshin1999@mail.ru>
2021-05-18 19:00:39 +0300

    Task 1

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -6,37 +6,37 @@ import math
 
 def mul(x, y):
     ":math:`f(x, y) = x * y`"
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return x * y
 
 
 def id(x):
     ":math:`f(x) = x`"
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return x
 
 
 def add(x, y):
     ":math:`f(x, y) = x + y`"
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return x + y
 
 
 def neg(x):
     ":math:`f(x) = -x`"
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return -x
 
 
 def lt(x, y):
     ":math:`f(x) =` 1.0 if x is less than y else 0.0"
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return 1.0 if x < y else 0.0
 
 
 def eq(x, y):
     ":math:`f(x) =` 1.0 if x is equal to y else 0.0"
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return 1.0 if x == y else 0.0
 
 
 def max(x, y):
     ":math:`f(x) =` x if x is greater than y else y"
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return x if x > y else y
 
 
 def sigmoid(x):
@@ -52,7 +52,11 @@ def sigmoid(x):
     for stability.
 
     """
-    raise NotImplementedError('Need to include this file from past assignment.')
+    if x >= 0.0:
+        return 1.0 / (1.0 + math.exp(-x))
+
+    e = math.exp(x)
+    return e / (1.0 + e)
 
 
 def relu(x):
@@ -61,12 +65,12 @@ def relu(x):
 
     (See `<https://en.wikipedia.org/wiki/Rectifier_(neural_networks)>`_ .)
     """
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return x if x > 0 else 0.0
 
 
 def relu_back(x, y):
     ":math:`f(x) =` y if x is greater than 0 else 0"
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return y if x > 0.0 else 0.0
 
 
 EPS = 1e-6
@@ -109,13 +113,13 @@ def map(fn):
     See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
 
     Args:
-        fn (one-arg function): Function from one value to one value.
+        fn (one-arg function): process one value
 
     Returns:
-        function : A function that takes a list, applies `fn` to each element, and returns a
-        new list
+        function : a function that takes a list and applies `fn` to each element
     """
-    raise NotImplementedError('Need to include this file from past assignment.')
+    # TODO: Implement for Task 0.3.
+    raise NotImplementedError("Need to implement for Task 0.3")
 
 
 def negList(ls):
@@ -139,7 +143,7 @@ def zipWith(fn):
         applying fn(x, y) one each pair of elements.
 
     """
-    raise NotImplementedError('Need to include this file from past assignment.')
+    return lambda x, y: [fn(*z) for z in zip(x, y)]
 
 
 def addLists(ls1, ls2):
@@ -164,18 +168,26 @@ def reduce(fn, start):
         fn(x_1, x_0)))`
 
     """
-    raise NotImplementedError('Need to include this file from past assignment.')
+
+    def reduce_core(ls, y=start):
+        for x in ls:
+            y = fn(x, y)
+        return y
+
+    return reduce_core
 
 
 def sum(ls):
     """
     Sum up a list using :func:`reduce` and :func:`add`.
     """
-    raise NotImplementedError('Need to include this file from past assignment.')
+    fn = reduce(add, 0.0)
+    return fn(ls)
 
 
 def prod(ls):
     """
     Product of a list using :func:`reduce` and :func:`mul`.
     """
-    raise NotImplementedError('Need to include this file from past assignment.')
+    fn = reduce(mul, 1.0)
+    return fn(ls)
